414e0fe882244db00887d3a90983ffdf6d420f36
miku03090831 <1965489500@qq.com>
2021-06-23 20:02:57 +0800

    add number of search results
    增加了搜索结果条数的返回提示

--- src/components/Food.vue
+++ src/components/Food.vue
@@ -14,6 +14,7 @@
       <el-input v-model="input" placeholder="请输入内容"></el-input>
       <el-button type="primary">搜索</el-button>
     </div>
+    <div class="resultnum">搜索共得到 {{ len }} 条结果</div>
     <div class="food-list">
       <el-scrollbar>
         <div>
@@ -22,13 +23,13 @@
               <el-card class="food-card">
                 <div class="content">
                   <div slot="header" class="clearfix">
-                    <span>{{searchResult[index].name}}</span>
+                    <span>{{ searchResult[index].name }}</span>
                   </div>
                   <div class="nutrition">
-                    {{searchResult[index].nutrition}}
+                    {{ searchResult[index].nutrition }}
                   </div>
                   <div class="avoid">
-                    {{searchResult[index].avoid}}
+                    {{ searchResult[index].avoid }}
                   </div>
                 </div>
                 <!-- <div class="pic">
@@ -53,7 +54,7 @@ export default {
   name: "Food",
   data() {
     return {
-      input:"",
+      input: "",
       count: 0,
       options: [
         {
@@ -74,66 +75,70 @@ export default {
         },
       ],
       value: "",
-      searchResult:[{
-        name:"猪肉1",
-        nutrition:"营养含量（每100g）：热量143千卡，钙6毫克，蛋白质20.3克",
-        avoid:"食用禁忌：不宜同时大量饮茶，不宜多食煎炸咸肉"
-      },{
-        name:"猪肉2",
-        nutrition:"营养含量（每100g）：热量143千卡，钙6毫克，蛋白质20.3克",
-        avoid:"食用禁忌：不宜同时大量饮茶，不宜多食煎炸咸肉"
-      },
-      {
-        name:"猪肉3",
-        nutrition:"营养含量（每100g）：热量143千卡，钙6毫克，蛋白质20.3克",
-        avoid:"食用禁忌：不宜同时大量饮茶，不宜多食煎炸咸肉"
-      },
-      {
-        name:"猪肉4",
-        nutrition:"营养含量（每100g）：热量143千卡，钙6毫克，蛋白质20.3克",
-        avoid:"食用禁忌：不宜同时大量饮茶，不宜多食煎炸咸肉"
-      },
-      {
-        name:"猪肉5",
-        nutrition:"营养含量（每100g）：热量143千卡，钙6毫克，蛋白质20.3克",
-        avoid:"食用禁忌：不宜同时大量饮茶，不宜多食煎炸咸肉"
-      },
-      {
-        name:"猪肉6",
-        nutrition:"营养含量（每100g）：热量143千卡，钙6毫克，蛋白质20.3克",
-        avoid:"食用禁忌：不宜同时大量饮茶，不宜多食煎炸咸肉"
-      },
-      {
-        name:"猪肉7",
-        nutrition:"营养含量（每100g）：热量143千卡，钙6毫克，蛋白质20.3克",
-        avoid:"食用禁忌：不宜同时大量饮茶，不宜多食煎炸咸肉"
-      },
-      {
-        name:"猪肉8",
-        nutrition:"营养含量（每100g）：热量143千卡，钙6毫克，蛋白质20.3克",
-        avoid:"食用禁忌：不宜同时大量饮茶，不宜多食煎炸咸肉"
-      },
-      {
-        name:"猪肉9",
-        nutrition:"营养含量（每100g）：热量143千卡，钙6毫克，蛋白质20.3克",
-        avoid:"食用禁忌：不宜同时大量饮茶，不宜多食煎炸咸肉"
-      },
-      {
-        name:"猪肉10",
-        nutrition:"营养含量（每100g）：热量143千卡，钙6毫克，蛋白质20.3克",
-        avoid:"食用禁忌：不宜同时大量饮茶，不宜多食煎炸咸肉"
-      },
-      {
-        name:"猪肉11",
-        nutrition:"营养含量（每100g）：热量143千卡，钙6毫克，蛋白质20.3克",
-        avoid:"食用禁忌：不宜同时大量饮茶，不宜多食煎炸咸肉"
-      }]
+      searchResult: [
+        {
+          name: "猪肉1",
+          nutrition: "营养含量（每100g）：热量143千卡，钙6毫克，蛋白质20.3克",
+          avoid: "食用禁忌：不宜同时大量饮茶，不宜多食煎炸咸肉",
+        },
+        {
+          name: "猪肉2",
+          nutrition: "营养含量（每100g）：热量143千卡，钙6毫克，蛋白质20.3克",
+          avoid: "食用禁忌：不宜同时大量饮茶，不宜多食煎炸咸肉",
+        },
+        {
+          name: "猪肉3",
+          nutrition: "营养含量（每100g）：热量143千卡，钙6毫克，蛋白质20.3克",
+          avoid: "食用禁忌：不宜同时大量饮茶，不宜多食煎炸咸肉",
+        },
+        {
+          name: "猪肉4",
+          nutrition: "营养含量（每100g）：热量143千卡，钙6毫克，蛋白质20.3克",
+          avoid: "食用禁忌：不宜同时大量饮茶，不宜多食煎炸咸肉",
+        },
+        {
+          name: "猪肉5",
+          nutrition: "营养含量（每100g）：热量143千卡，钙6毫克，蛋白质20.3克",
+          avoid: "食用禁忌：不宜同时大量饮茶，不宜多食煎炸咸肉",
+        },
+        {
+          name: "猪肉6",
+          nutrition: "营养含量（每100g）：热量143千卡，钙6毫克，蛋白质20.3克",
+          avoid: "食用禁忌：不宜同时大量饮茶，不宜多食煎炸咸肉",
+        },
+        {
+          name: "猪肉7",
+          nutrition: "营养含量（每100g）：热量143千卡，钙6毫克，蛋白质20.3克",
+          avoid: "食用禁忌：不宜同时大量饮茶，不宜多食煎炸咸肉",
+        },
+        {
+          name: "猪肉8",
+          nutrition: "营养含量（每100g）：热量143千卡，钙6毫克，蛋白质20.3克",
+          avoid: "食用禁忌：不宜同时大量饮茶，不宜多食煎炸咸肉",
+        },
+        {
+          name: "猪肉9",
+          nutrition: "营养含量（每100g）：热量143千卡，钙6毫克，蛋白质20.3克",
+          avoid: "食用禁忌：不宜同时大量饮茶，不宜多食煎炸咸肉",
+        },
+        {
+          name: "猪肉10",
+          nutrition: "营养含量（每100g）：热量143千卡，钙6毫克，蛋白质20.3克",
+          avoid: "食用禁忌：不宜同时大量饮茶，不宜多食煎炸咸肉",
+        },
+        {
+          name: "猪肉11",
+          nutrition: "营养含量（每100g）：热量143千卡，钙6毫克，蛋白质20.3克",
+          avoid: "食用禁忌：不宜同时大量饮茶，不宜多食煎炸咸肉",
+        },
+      ],
     };
   },
-  computed:{
-    len(){
-      return this.searchResult.length;
-    }
+  computed: {
+    len() {
+      if (this.searchResult) return this.searchResult.length;
+      else return 0;
+    },
   },
   methods: {
     // load() {
@@ -160,10 +165,16 @@ export default {
   margin: 0 10px;
 }
 
+.resultnum{
+  position: absolute;
+  top: 15%;
+  left: 10%;
+}
+
 .food-list {
   position: absolute;
-  height: 85%;
-  top: 15%;
+  height: 80%;
+  top: 20%;
   width: 80%;
   left: 10%;
 }
